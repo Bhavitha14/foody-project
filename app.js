76fa091a611c7c8c39239f62fbe0503fdf6c4227
Bhavitha14 <139752681+Bhavitha14@users.noreply.github.com>
2024-07-18 14:01:31 +0530

    completed 3 sections

---
+++ app.js
@@ -0,0 +1,8 @@
+var menuBtn=document.querySelector(".navbar-toggler");//icon
+var menu=document.querySelector(".menu-items");//menu
+menuBtn.addEventListener("click",function(){
+    menu.classList.toggle("active");
+})
+menuCls.addEventListener("click",function(){
+    menu.classList.toggle("active");
+})
